76870268258d683ae9a583205a649339d354db90
vorgestern <vorgestern@niemals>
2024-12-24 01:57:49 +0100

    Indentation

--- src/euler_to_axisangle.cpp
+++ src/euler_to_axisangle.cpp
@@ -13,61 +13,61 @@ using namespace glm;
 #if 0
 void ZXYAngleToAxisAngle(double axis[], double*angle, double xangle, double yangle, double zangle)
 {
-  const dmat4 MX=eulerAngleX(xangle), MY=eulerAngleY(-yangle), MZ=eulerAngleZ(zangle);
-  const dmat4 M=MZ*MX*MY;
-  dvec3 myaxis;
-  double myangle;
-  axisAngle(M, myaxis, myangle);
-  axis[0]=myaxis.x;
-  axis[1]=myaxis.y;
-  axis[2]=myaxis.z;
-  *angle=myangle;
+    const dmat4 MX=eulerAngleX(xangle), MY=eulerAngleY(-yangle), MZ=eulerAngleZ(zangle);
+    const dmat4 M=MZ*MX*MY;
+    dvec3 myaxis;
+    double myangle;
+    axisAngle(M, myaxis, myangle);
+    axis[0]=myaxis.x;
+    axis[1]=myaxis.y;
+    axis[2]=myaxis.z;
+    *angle=myangle;
 }
 
 void ZYXAngleToAxisAngle(double axis[], double*angle, double xangle, double yangle, double zangle)
 {
-  const dmat4 MX=eulerAngleX(xangle), MY=eulerAngleY(-yangle), MZ=eulerAngleZ(zangle);
-  const dmat4 M=MZ*MY*MX;
-  dvec3 axis2;
-  double angle2;
-  axisAngle(M, axis2, angle2);
-  axis[0]=axis2.x;
-  axis[1]=axis2.y;
-  axis[2]=axis2.z;
-  *angle=angle2;
+    const dmat4 MX=eulerAngleX(xangle), MY=eulerAngleY(-yangle), MZ=eulerAngleZ(zangle);
+    const dmat4 M=MZ*MY*MX;
+    dvec3 axis2;
+    double angle2;
+    axisAngle(M, axis2, angle2);
+    axis[0]=axis2.x;
+    axis[1]=axis2.y;
+    axis[2]=axis2.z;
+    *angle=angle2;
 }
 #endif
 
 void AnglesToAxisAngle(double axis[], double*angle, const double A[], const int D[], unsigned num)
 {
-  dvec3 myaxis {0,0,1};
-  double myangle=0;
-  switch (num)
-  {
-    case 1:
+    dvec3 myaxis {0,0,1};
+    double myangle=0;
+    switch (num)
     {
-      const dmat4 M=D[0]==0?eulerAngleX(A[0]):D[0]==1?eulerAngleY(A[0]):eulerAngleZ(A[0]);
-      axisAngle(M, myaxis, myangle);
-      break;
-    }
-    case 2:
+        case 1:
     {
-      const dmat4 M1=D[0]==0?eulerAngleX(A[0]):D[0]==1?eulerAngleY(A[0]):eulerAngleZ(A[0]);
-      const dmat4 M2=D[1]==0?eulerAngleX(A[1]):D[1]==1?eulerAngleY(A[1]):eulerAngleZ(A[1]);
-      axisAngle(M1*M2, myaxis, myangle);
-      break;
-    }
-    case 3:
+            const dmat4 M=D[0]==0?eulerAngleX(A[0]):D[0]==1?eulerAngleY(A[0]):eulerAngleZ(A[0]);
+            axisAngle(M, myaxis, myangle);
+            break;
+        }
+        case 2:
     {
-      const dmat4 M1=D[0]==0?eulerAngleX(A[0]):D[0]==1?eulerAngleY(A[0]):eulerAngleZ(A[0]);
-      const dmat4 M2=D[1]==0?eulerAngleX(A[1]):D[1]==1?eulerAngleY(A[1]):eulerAngleZ(A[1]);
-      const dmat4 M3=D[2]==0?eulerAngleX(A[2]):D[2]==1?eulerAngleY(A[2]):eulerAngleZ(A[2]);
-      axisAngle(M1*M2*M3, myaxis, myangle);
-      break;
+            const dmat4 M1=D[0]==0?eulerAngleX(A[0]):D[0]==1?eulerAngleY(A[0]):eulerAngleZ(A[0]);
+            const dmat4 M2=D[1]==0?eulerAngleX(A[1]):D[1]==1?eulerAngleY(A[1]):eulerAngleZ(A[1]);
+            axisAngle(M1*M2, myaxis, myangle);
+            break;
+        }
+        case 3:
+    {
+            const dmat4 M1=D[0]==0?eulerAngleX(A[0]):D[0]==1?eulerAngleY(A[0]):eulerAngleZ(A[0]);
+            const dmat4 M2=D[1]==0?eulerAngleX(A[1]):D[1]==1?eulerAngleY(A[1]):eulerAngleZ(A[1]);
+            const dmat4 M3=D[2]==0?eulerAngleX(A[2]):D[2]==1?eulerAngleY(A[2]):eulerAngleZ(A[2]);
+            axisAngle(M1*M2*M3, myaxis, myangle);
+            break;
+        }
     }
-  }
-  axis[0]=myaxis.x;
-  axis[1]=myaxis.y;
-  axis[2]=myaxis.z;
-  *angle=myangle;
+    axis[0]=myaxis.x;
+    axis[1]=myaxis.y;
+    axis[2]=myaxis.z;
+    *angle=myangle;
 }
--- src/x3d.cpp
+++ src/x3d.cpp
@@ -8,98 +8,98 @@ using namespace std;
 
 void dumphumanoid_x3d(const vector<hanimjoint>&JOINTS, SegmentForms segmentshape)
 {
-  const auto NJ=JOINTS.size();
-  int merklev=0;
-  unsigned nummat=0;
-  for (unsigned n=0; n<NJ; n++)
-  {
-    const hanimjoint&J=JOINTS[n];
-    const int lev=level(J);
-    const double*T=offset(J);
-    const char*nam=name(J);
-
-    // Upon rising up again in the hierarchy, close open joints.
-    bool up=n>0 && lev<merklev;
-    if (up)
+    const auto NJ=JOINTS.size();
+    int merklev=0;
+    unsigned nummat=0;
+    for (unsigned n=0; n<NJ; n++)
     {
-      for (int d=merklev; d>=lev; d--)
-        printf("\n%*s</%s>", 2*d, "", d>0?"Transform":"Transform");
-    }
+        const hanimjoint&J=JOINTS[n];
+        const int lev=level(J);
+        const double*T=offset(J);
+        const char*nam=name(J);
 
-    switch (type(J))
-    {
-      case jtype::root: break;
-      // Joints and End Sites
-      case jtype::joint:
-      case jtype::endsite:
-      {
-        switch (segmentshape)
+        // Upon rising up again in the hierarchy, close open joints.
+        bool up=n>0 && lev<merklev;
+        if (up)
         {
-          case SegmentForms::none: break;
-          case SegmentForms::line:
-          {
-            // Draw a line
-            if (T!=nullptr) printf("\n%*s<Shape><LineSet vertexCount='2'><Coordinate point='0 0 0,%g %g %g'/></LineSet></Shape>", 2*lev, "", T[0],T[1],T[2]);
-            break;
-          }
-          case SegmentForms::cylinder:
-          {
-            // Zeichne einen Zylinder
-            if (T!=nullptr)
-            {
-              const double hei=sqrt(T[0]*T[0]+T[1]*T[1]+T[2]*T[2]);
-              const double rad=0.1*sqrt(T[0]*T[0]+T[1]*T[1]+T[2]*T[2]);
-              const double xangle=atan2(T[2],T[1]), zangle=atan2(T[0],T[1]);
-              if (xangle!=0 || zangle!=0)
-              {
-                const double ANGLE[2]={xangle,zangle};
-                const int DIR[2]={0,2};
-                double axis[3], angle;
-                AnglesToAxisAngle(axis,&angle,ANGLE,DIR,2);
-                printf("\n<Transform translation='%g %g %g' rotation='%g %g %g %g'>"
-                , .5*T[0],.5*T[1],.5*T[2]
-                , axis[0],axis[1],axis[2], angle);
-              }
-              else
-              {
-                printf("\n<Transform translation='%g %g %g'>", .5*T[0],.5*T[1],.5*T[2]);
-              }
-              if (nummat==0) printf("\n<Shape><Appearance DEF='app1'><Material DEF='mat1' diffuseColor='1 1 1' specularColor='1 1 1' shininess='1.0'/></Appearance>");
-              else           printf("\n<Shape><Appearance USE='app1'/>");
-              printf("<Cylinder height='%g' radius='%g'/></Shape>", hei, rad);
-              printf("</Transform>");
-              nummat++;
-            }
-            break;
-          }
+            for (int d=merklev; d>=lev; d--)
+                printf("\n%*s</%s>", 2*d, "", d>0?"Transform":"Transform");
         }
-        break;
-      }
-    }
 
-    printf("\n%*s<%s", 2*lev, "", lev>0?"Transform":"Transform");
-    if (T!=nullptr) printf(" translation='%g %g %g'", T[0],T[1],T[2]);
-    if (nam!=nullptr)
-    {
-      printf(" DEF='%s'>", nam);
+        switch (type(J))
+        {
+            case jtype::root: break;
+                // Joints and End Sites
+            case jtype::joint:
+            case jtype::endsite:
+            {
+                switch (segmentshape)
+                {
+                    case SegmentForms::none: break;
+                    case SegmentForms::line:
+                    {
+                        // Draw a line
+                        if (T!=nullptr) printf("\n%*s<Shape><LineSet vertexCount='2'><Coordinate point='0 0 0,%g %g %g'/></LineSet></Shape>", 2*lev, "", T[0],T[1],T[2]);
+                        break;
+                    }
+                    case SegmentForms::cylinder:
+                    {
+                        // Zeichne einen Zylinder
+                        if (T!=nullptr)
+                        {
+                            const double hei=sqrt(T[0]*T[0]+T[1]*T[1]+T[2]*T[2]);
+                            const double rad=0.1*sqrt(T[0]*T[0]+T[1]*T[1]+T[2]*T[2]);
+                            const double xangle=atan2(T[2],T[1]), zangle=atan2(T[0],T[1]);
+                            if (xangle!=0 || zangle!=0)
+                            {
+                                const double ANGLE[2]={xangle,zangle};
+                                const int DIR[2]={0,2};
+                                double axis[3], angle;
+                                AnglesToAxisAngle(axis,&angle,ANGLE,DIR,2);
+                                printf("\n<Transform translation='%g %g %g' rotation='%g %g %g %g'>"
+                                    , .5*T[0],.5*T[1],.5*T[2]
+                                    , axis[0],axis[1],axis[2], angle);
+                            }
+                            else
+                            {
+                                printf("\n<Transform translation='%g %g %g'>", .5*T[0],.5*T[1],.5*T[2]);
+                            }
+                            if (nummat==0) printf("\n<Shape><Appearance DEF='app1'><Material DEF='mat1' diffuseColor='1 1 1' specularColor='1 1 1' shininess='1.0'/></Appearance>");
+                            else           printf("\n<Shape><Appearance USE='app1'/>");
+                            printf("<Cylinder height='%g' radius='%g'/></Shape>", hei, rad);
+                            printf("</Transform>");
+                            nummat++;
+                        }
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+
+        printf("\n%*s<%s", 2*lev, "", lev>0?"Transform":"Transform");
+        if (T!=nullptr) printf(" translation='%g %g %g'", T[0],T[1],T[2]);
+        if (nam!=nullptr)
+        {
+            printf(" DEF='%s'>", nam);
+        }
+        else
+        {
+            printf(">");
+        }
+        const double rad=T==nullptr?1:0.10*sqrt(T[0]*T[0]+T[1]*T[1]+T[2]*T[2]);
+        if (nummat==0)
+        {
+            printf("<Shape><Appearance DEF='app1'><Material DEF='mat1' diffuseColor='1 1 1' specularColor='1 1 1' shininess='1.0'/></Appearance><Sphere radius='%g'/></Shape>", rad);
+        }
+        else
+        {
+            printf("<Shape><Appearance USE='app1'/><Sphere radius='%g'/></Shape>", rad);
+        }
+        nummat++;
+        merklev=level(J);
     }
-    else
-    {
-      printf(">");
-    }
-    const double rad=T==nullptr?1:0.10*sqrt(T[0]*T[0]+T[1]*T[1]+T[2]*T[2]);
-    if (nummat==0)
-    {
-      printf("<Shape><Appearance DEF='app1'><Material DEF='mat1' diffuseColor='1 1 1' specularColor='1 1 1' shininess='1.0'/></Appearance><Sphere radius='%g'/></Shape>", rad);
-    }
-    else
-    {
-      printf("<Shape><Appearance USE='app1'/><Sphere radius='%g'/></Shape>", rad);
-    }
-    nummat++;
-    merklev=level(J);
-  }
-  for (int d=merklev; d>=0; d--) printf("\n%*s</%s>", 2*d, "", d>0?"Transform":"Transform");
+    for (int d=merklev; d>=0; d--) printf("\n%*s</%s>", 2*d, "", d>0?"Transform":"Transform");
 }
 
 void dumpmotiontable_x3d(const vector<hanimjoint>&joints, const vector<vector<double>>&Table)
